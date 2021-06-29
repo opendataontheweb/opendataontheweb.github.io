4506c5a46dce571a96315cfffed13bb8cc7566da
opendataontheweb <85843648+opendataontheweb@users.noreply.github.com>
2021-06-29 17:57:19 +0500

    Create classifier.js

---
+++ static/classifier.js
@@ -0,0 +1,67 @@
+//<script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@2.0.0/dist/tf.min.js"></script>
+var model;
+var img_url;
+var loaded = false;
+
+async function load(){
+
+    model = await tf.loadLayersModel('alexnet_xray_js/model.json');
+    loaded = true;
+
+}
+
+load();
+
+function LoadFile(event){
+    img_url = URL.createObjectURL(event.target.files[0]);
+    console.log(img_url);
+}
+function predict(){
+
+    if(loaded){
+
+        var image = new Image();
+
+        image.src = img_url;
+
+        image.onload = () =>{
+
+        var img_tensor = tf.browser.fromPixels(image).resizeBilinear([227, 227]).div(tf.scalar(255));
+
+        img_tensor = tf.expandDims(img_tensor, 0);
+
+        console.log(img_tensor.shape);
+
+        var pred = model.predict(img_tensor).dataSync()[0];
+
+        pred = pred*100;
+
+        pred = pred.toFixed(2);
+
+        console.log(pred);
+
+        res = document.getElementById('result');
+
+        if(pred>70){
+            res.innerHTML = "PNEUMONIA: "+pred.toString()+"%";
+        }
+
+        else if (pred<30){
+            res.innerHTML = "NORMAL: "+(100-pred).toString()+"%";
+        }
+
+        else{
+
+            res.innerHTML = "UNKNOWN";
+
+        }
+
+        }
+
+    }
+
+    else{
+        alert('Prediction model has not loaded yet, please try again in a few seconds');
+    }
+
+}
